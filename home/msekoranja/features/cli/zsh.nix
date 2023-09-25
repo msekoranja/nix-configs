77c66656b8ecb95d8b894e598c34154b7eba0eb4
msekoranja <matej.sekoranja@gmail.com>
2023-09-25 11:11:53 +0200

    zsh slow paste fix

--- home/msekoranja/features/cli/zsh.nix
+++ home/msekoranja/features/cli/zsh.nix
@@ -33,21 +33,22 @@
             echo "'$1' is not a valid file!"
           fi
         }'';
-
-        envExtra = ''
-          ### Fix slowness of pastes with zsh-syntax-highlighting.zsh
-          pasteinit() {
-            OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
-            zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
-          }
-
-          pastefinish() {
-            zle -N self-insert $OLD_SELF_INSERT
-          }
-          zstyle :bracketed-paste-magic paste-init pasteinit
-          zstyle :bracketed-paste-magic paste-finish pastefinish
-          ### Fix slowness of pastes
-        '';
     };
+
+    envExtra = ''
+      ### Fix slowness of pastes with zsh-syntax-highlighting.zsh
+      pasteinit() {
+        OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
+        zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+      }
+
+      pastefinish() {
+        zle -N self-insert ''$OLD_SELF_INSERT
+      }
+      zstyle :bracketed-paste-magic paste-init pasteinit
+      zstyle :bracketed-paste-magic paste-finish pastefinish
+      ### Fix slowness of pastes
+    '';
+
   };
 }
