c175c33af24e0fa54cc398fd8848effaafe5eca9
Matej Sekoranja <matej.sekoranja@gmail.com>
2023-09-26 00:36:09 +0200

    fzf ctrl+t tuning

--- home/msekoranja/features/cli/fzf.nix
+++ home/msekoranja/features/cli/fzf.nix
@@ -1,5 +1,10 @@
 {
   programs.fzf = {
     enable = true;
+    # CTRL+T (shamefully assumes bat, exa)
+    fileWidgetOptions = [
+      "--preview '(bat -p --color=always {} || exa --tree --level=4 --color=always {}) 2> /dev/null | head -200'"
+      "--bind 'ctrl-t:change-preview-window(down|hidden|)'"
+    ];
   };
 }
